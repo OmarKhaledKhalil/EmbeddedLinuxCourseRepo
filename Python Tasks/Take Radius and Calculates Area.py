527fb1ff5b5f7e9724a01250a123ee367baea252
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-08-10 02:44:17 +0300

    Add files via upload

---
+++ Python Tasks/Take Radius and Calculates Area.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#include Math Module to get PI Value and use it
+import math
+
+#Input Radius by User
+radius=(float)(input("Enter The Radius in cm"))
+
+#Area Calculations 
+area=math.pi*radius*radius
+
+print(f"Area is '{area}' cm Squares")
