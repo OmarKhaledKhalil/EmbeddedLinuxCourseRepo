0f0d6e97a1374927189bc007d76fc74547307e01
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-08-12 14:38:57 +0300

    Add files via upload

---
+++ Python Tasks/Check Username and Password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#Take the input username and password from the user
+
+user_name=input("Please Enter the User Name: ")
+password=input("Please Enther The Password: ")
+
+if user_name=="Omar" and password=="123":
+    print("Welcome "+user_name)
+
