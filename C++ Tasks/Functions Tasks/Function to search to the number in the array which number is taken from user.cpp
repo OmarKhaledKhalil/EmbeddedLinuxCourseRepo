6e2872536d9c2d3e70a39cfb7b051d1a1e226162
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-09-07 14:43:41 +0300

    Update Function to search to the number in the array which number is taken from user.cpp

--- C++ Tasks/Functions Tasks/Function to search to the number in the array which number is taken from user.cpp
+++ C++ Tasks/Functions Tasks/Function to search to the number in the array which number is taken from user.cpp
@@ -1,25 +1,25 @@
-int Array_int_CheckNumber(int arr[], int copy_ArrLength , int copy_NumberToSearch , int * Element_Number_Ref )
+int Array_int_CheckNumber( int arr[] , int copy_ArrLength , int copy_NumberToSearch , int * Element_Number_Ref )
 
 {
    
  
      // Searching  for  the  recieved  number  in  the  array  elements.
 
-    for( int  i=0 ; i<copy_ArrLength ; i++ )
+    for( int  i=0 ; i < copy_ArrLength ; i++ )
     
     {
-        if(arr[i]==copy_NumberToSearch)
+        if( arr[i] == copy_NumberToSearch )
         {
 
-            *Element_Number_Ref=i; // Passing  Index  of  This  element.
+            *Element_Number_Ref = i ; // Passing  Index  of  This  element.
 
-            return 1 ;             // Number  Exists   in   the   Array.
+            return 1  ;               // Number  Exists   in   the   Array.
 
         }
 
     }
 
-            return -1 ;            // Number doesn't exist in the Array.
+            return -1 ;               // Number doesn't exist in the Array.
 
 }
 
