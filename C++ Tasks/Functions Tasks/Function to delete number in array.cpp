5211c4b92901a5e841a3f404347d80901d6cb323
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-09-07 14:41:35 +0300

    Update Function to delete number in array.cpp

--- C++ Tasks/Functions Tasks/Function to delete number in array.cpp
+++ C++ Tasks/Functions Tasks/Function to delete number in array.cpp
@@ -1,4 +1,4 @@
-int Array_int_DeleteNumber(int arr[], int copy_ArrLength , int copy_NumberToSearch , int ** Ptr_New_Array)
+int Array_int_DeleteNumber( int arr[] , int copy_ArrLength , int copy_NumberToSearch , int ** Ptr_New_Array )
 
 {
 
@@ -6,31 +6,31 @@ int Array_int_DeleteNumber(int arr[], int copy_ArrLength , int copy_NumberToSear
 
              // Searching  for  the  recieved  number  in  the  array  elements.
 
-    for( i=0 ; i<copy_ArrLength ; i++ )
+    for( i = 0 ; i < copy_ArrLength ; i++ )
     
     {
-        if(arr[i]==copy_NumberToSearch)
+        if( arr[i] == copy_NumberToSearch )
         
         {
             //If Number is Found , I allocate new heap locations for the new array 
             //With length Less than the old array  by 1
 
 
-            *Ptr_New_Array = (int*)malloc( (copy_ArrLength-1) * (sizeof(int)) );
+            *Ptr_New_Array = (int*) malloc ( (copy_ArrLength-1) * ( sizeof(int) ) );
              
-             i=0;
+             i = 0 ;
 
-             k=0;
+             k = 0 ;
 
 
             // Loop to copy the old array to the new array unless the target number.
 
 
-            while(i<copy_ArrLength)
+            while ( i < copy_ArrLength )
     
                {
         
-                  if(arr[i]==copy_NumberToSearch)
+                  if( arr[i] == copy_NumberToSearch )
             
                    {
                
@@ -39,13 +39,13 @@ int Array_int_DeleteNumber(int arr[], int copy_ArrLength , int copy_NumberToSear
             
                    {
 
-                      *((*Ptr_New_Array)+k)= arr [i];  // Assigning the element of the new array in heap.
+                      * ( (*Ptr_New_Array) + k ) = arr [i] ;  // Assigning the element of the new array in heap.
             
-                       k++;
+                       k++ ;
 
                    }
                      
-                      i++;
+                      i++  ;
 
                 }     
 
@@ -56,9 +56,9 @@ int Array_int_DeleteNumber(int arr[], int copy_ArrLength , int copy_NumberToSear
     }
 
    
-   *Ptr_New_Array = nullptr;
+   *Ptr_New_Array = nullptr ;
 
-   return 0;                            // Return 0 operation isn't okay.
+   return 0 ;                            // Return 0 operation isn't okay.
 
 
 
