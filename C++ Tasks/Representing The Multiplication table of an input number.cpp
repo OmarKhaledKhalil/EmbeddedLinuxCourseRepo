84c103891fc9c506f553cbd14091bac92654ebcd
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-09-07 14:32:18 +0300

    Update Representing The Multiplication table of an input number.cpp

--- C++ Tasks/Representing The Multiplication table of an input number.cpp
+++ C++ Tasks/Representing The Multiplication table of an input number.cpp
@@ -8,7 +8,7 @@ int main ()
 
    //Super Loop Program System.
 
-   while(1)
+   while ( 1 )
    {
 
     //Recieving the Input Number and store it in the Variable in RAM  .
@@ -19,53 +19,53 @@ int main ()
     //Representing Multiplication Table of the Input Number as well as
     //Doing some if conidtions to  Refining  the  form  of  the Table.
 
-      for( int i = 0 ; i<10 ; i++ )
+      for( int i = 0 ; i < 10 ; i++ )
       
       {
         
-        if((num*i)%10==(num*i))
+        if( ( num * i ) %10 == ( num * i ) )
        
         {
-           printf("|   %d    x    %d    |    %d     |\n",num,i,num*i); 
+           printf("|   %d    x    %d    |    %d     |\n", num , i , num*i ); 
         }
 
-        else if((num*i)/100000!=0)
+        else if( ( num * i ) / 00000 != 0)
         
         {
 
-           printf("|   %d    x    %d    |  %d  |\n",num,i,num*i);
+           printf("|   %d    x    %d    |  %d  |\n", num , i , num*i );
 
         }
 
-        else if((num*i)/10000!=0)
+        else if( ( num * i ) / 10000 != 0 )
 
         {
 
-           printf("|   %d    x    %d    |   %d  |\n",num,i,num*i);
+           printf("|   %d    x    %d    |   %d  |\n", num , i , num*i );
 
         }
 
-        else if((num*i)/1000!=0)
+        else if( ( num * i ) / 1000 != 0 )
         
         {
    
-           printf("|   %d    x    %d    |   %d   |\n",num,i,num*i);
+           printf("|   %d    x    %d    |   %d   |\n", num , i , num*i );
 
         }
 
 
-        else if((num*i)/100!=0)
+        else if( ( num * i ) / 100 != 0 )
 
         {
 
-          printf("|   %d    x    %d    |    %d   |\n",num,i,num*i);
+          printf("|   %d    x    %d    |    %d   |\n", num , i , num*i );
 
         }
 
         else
         {
 
-          printf("|   %d    x    %d    |    %d    |\n",num,i,num*i);
+          printf("|   %d    x    %d    |    %d    |\n", num , i, num* i);
 
         }
         
