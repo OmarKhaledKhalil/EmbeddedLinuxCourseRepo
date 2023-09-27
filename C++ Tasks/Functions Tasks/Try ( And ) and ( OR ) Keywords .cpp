4e79bdaf6a5bfdf60bef100c9be560ceddf929e2
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-09-27 20:56:59 +0300

    Add files via upload

---
+++ C++ Tasks/Functions Tasks/Try ( And ) and ( OR ) Keywords .cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+   int x=10;
+
+   if(x<10 and x>9)
+   
+   {
+
+    cout << "And_1 is working" << endl ; 
+
+   }
+
+   if ( x < 20 or x > 100 )
+   
+   {
+
+    cout << "OR_1 is working" << endl ;
+
+   }
+
+   if ( x<11 and x>9 )
+   
+   {
+
+    cout << "And_2 is working" << endl ; 
+
+   }
+   
+
+    return 0;
+}
